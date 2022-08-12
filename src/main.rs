be34ef44953b0c17408cafbf7e195be359523bc8
Léo Cazenave <leo@cazenave.cc>
2022-08-12 20:52:02 +0200

    Idiot proofing that actually works.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;           // command line args
 use std::fs;            // file manipulation
 use itertools::izip;    // zip more than 2 iterators
 use colored::Colorize;  // 'cause why not
+use regex::Regex;       // mainly for replace_all method
 
 fn get_contents(file: &String) -> String {
     fs::read_to_string(file).expect(format!("Couldn't read from file `{}`", file).as_str())
@@ -12,11 +13,23 @@ fn nb_chars(text: &String) -> usize {
     text_as_vec.len()
 }
 
-fn all_strings_same_length(input: Vec<&String>) -> Result<(), String> {
+fn compatible_files(input: Vec<&String>) -> Result<(), String> {
     assert!(!input.is_empty());
+
+    // Check if every files have the same number of characters
     let lenghts: Vec<usize> = input.iter().map(|x| nb_chars(x)).collect();
 
     if lenghts.iter().min() == lenghts.iter().max() {
+        // Check if new_line characters match across files
+        let regexp = Regex::new(r"[^\n]").unwrap();
+        let head = regexp.replace_all(input[0], " ");
+
+        for s in input[1..].iter() {
+            if regexp.replace_all(*s, " ") != head {
+                return Err(String::from("uuu"))
+            }
+        };
+
         Ok(())
     } else {
         let mut rv_string = String::new();
@@ -42,13 +55,12 @@ fn main() {
     };
 
     if let Some(bg_color_map) = maybe_bg_color_map {
-        match all_strings_same_length(vec![&base_ascii_art, &fg_color_map, &bg_color_map]) {
+        match compatible_files(vec![&base_ascii_art, &fg_color_map, &bg_color_map]) {
             Ok(_)    => (),
             Err(why) => panic!("Ascii art and color maps don't have the amount\
             of characters :\n{}\nHint : use `bat ASCII_ART | sd -p '[^ ]' '·' > FG_COLOR_MAP`
             \ror `bat ASCII_ART | sd -p '[^ ]' '-' > BG_COLOR_MAP` to generate color map templates.
             \r(replace fullcaps names with your file names of course)", why),
         }
-
     }
 }
